82e520524384acf5b91aab6d8b6217319329c280
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-05-11 22:04:23 -0300

    Adicionando demais dependencias

---
+++ SolrDotnetSample.Domain/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using poc_console_solr.Domain.Abstractions;
+
+namespace poc_console_solr.Domain.Entities
+{
+    public class Post : Entity<Guid>
+    {
+        public string Description { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime IsSold { get; set; }
+        public DateTime PostedDate { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+        
+        protected override void SetId(Guid id) => Guid.NewGuid();
+    }
+}
---
+++ SolrDotnetSample.Services/Abstractions/SolrServices/SolrIndexService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SolrNet;
+using SolrNet.Exceptions;
+
+namespace poc_console_solr.Services.Abstractions.SolrServices
+{
+    public abstract class SolrIndexService<T, TSolrOperations> : ISolrIndexService<T>
+        where TSolrOperations : ISolrOperations<T>
+    {
+        private readonly TSolrOperations _solr;
+
+        protected SolrIndexService(ISolrOperations<T> solr)
+        {
+            _solr = (TSolrOperations) solr;
+        }
+
+        public bool AddUpdate(T document)
+        {
+            try
+            {
+                // If the id already exists, the record is updated, otherwise added                         
+                var responseHeader = _solr.Add(document);
+                
+                if(responseHeader.Status != 0)
+                    throw new Exception("error");
+                    
+                _solr.Commit();
+                return true;
+            }
+            catch (SolrNetException ex)
+            {
+                //Log exception
+                throw ex;
+                return false;
+            }
+        }
+
+        public bool Delete(T document)
+        {
+            try
+            {
+                //Can also delete by id                
+                _solr.Delete(document);
+                _solr.Commit();
+                return true;
+            }
+            catch (SolrNetException ex)
+            {
+                //Log exception
+                return false;
+            }
+        }
+    }
+}
