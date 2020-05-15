0d9098141ad9319b2eacde44c544545435ed67cf
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-05-14 23:05:33 -0300

    Evoluindo entity

--- SolrDotnetSample.Domain/Abstractions/Entity.cs
+++ SolrDotnetSample.Domain/Abstractions/Entity.cs
@@ -1,9 +1,20 @@
+using System.Linq;
+using poc_console_solr.Domain.Notifications;
+
 namespace poc_console_solr.Domain.Abstractions
 {
     public abstract class Entity<TId>
         where TId : struct
     {
+        protected Entity()
+        {
+            Notification = new Notification();
+        }
+
         public TId Id { get; protected set; }
+        public INotification Notification { get; }
+        public bool Valid => Notification?.Errors?.Any() == false;
+
         protected abstract void SetId(TId id);
     }
 }
