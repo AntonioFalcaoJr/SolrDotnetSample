0ec061d1ca1e7a4d215d93eb16f0da7d936d8c3d
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-05-21 19:05:25 -0300

    Adicionando projeto de WebApi

---
+++ SolrDotnetSample.WebApi/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SolrDotnetSample.Repositories.IoC;
+using SolrDotnetSample.Services.IoC;
+
+namespace SolrDotnetSample.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddRepositories();
+            services.AddAutoMapper();
+            services.AddServices();
+
+            services.AddSolr(options =>
+            {
+                options.BaseAddress = Configuration["Solr:BaseAddress"];
+                options.Core = Configuration["Solr:Core"];
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
